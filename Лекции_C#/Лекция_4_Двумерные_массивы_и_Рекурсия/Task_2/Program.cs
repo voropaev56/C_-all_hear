9f70b456c356bb46d6105a72d0eb838bb7ec78cf
«Alexey» <voropaev56@mail.ru>
2022-07-10 13:50:21 +0500

    Добавлен семинар 4

---
+++ Лекции_C#/Лекция_4_Двумерные_массивы_и_Рекурсия/Task_2/Program.cs
@@ -0,0 +1,37 @@
+﻿// 
+
+int[,] pic = new int[,]
+{
+    {0, 0, 1, 0, 0 ........}
+    {0, 1, 1, 0, }
+ // нам задан массив с числами 0 и 1 где рисунок палец вверх например из единичек
+}
+
+void PrintImage(int[,] image)
+{
+    for (int i = 0; i < image.GetLength(0); i++)
+    {
+        for (int j = 0; j < matr.GetLength(0); j++)
+        {
+            if(image[i, j] == 0) Console.Write($"{image[i, j]} ");
+            else Console.Write($"+");
+        }
+        Console.WriteLine();
+    }
+}
+
+void c(int row, int col)
+{
+    if(pic[row, col] == 0)
+    {
+        pic[row, col] = 1;
+        FillImage(row-1, col);
+        FillImage(row, col-1);
+        FillImage(row+1, col);
+        FillImage(row, col+1);
+    }
+}
+
+PrintImage(pic);
+FillImage(13, 13);   // любая точка которая должна быть внутри контура рисунка
+PrintImage(pic);
