7045c490d5bea9eea305525233e851d14f859cb3
«Alexey» <voropaev56@mail.ru>
2022-07-18 00:08:18 +0500

    Для пуша 17.07-18.07

--- Семинары_C#/HW_6_SEM6/Task_1/Program.cs
+++ Семинары_C#/HW_6_SEM6/Task_1/Program.cs
@@ -2,3 +2,45 @@
 // Посчитайте, сколько чисел больше 0 ввёл пользователь.
 // 0, 7, 8, -2, -2 -> 2
 // 1, -7, 567, 89, 223-> 3
+
+
+int[] CreateArray(int size)
+{
+    int[] newArray = new int[size];
+
+    Console.WriteLine("Creating array here: ");
+    for(int i = 0; i < size; i++)
+    {
+        Console.Write($"Input {i + 1} element: ");
+        newArray[i] = Convert.ToInt32(Console.ReadLine());
+
+    }
+    Console.WriteLine();
+    return newArray;
+}
+
+void  ShowArray(int[] array)
+{
+    for(int i = 0; i < array.Length; i++)
+        Console.Write(array[i] + " ");
+
+    Console.WriteLine();
+}
+
+int NumMoreThenZero(int[] array)
+{
+    int count = 0;
+        for(int i = 0; i < array.Length; i++)
+           {
+           if(array[i] > 0) 
+           count = count + 1;
+           }
+    return count; 
+}
+
+Console.Write("Input size of array: ");
+int size = Convert.ToInt32(Console.ReadLine());
+int[] array1 = CreateArray(size);
+ShowArray(array1); 
+Console.Write("Numbers more then zero: " + NumMoreThenZero(array1));
+
---
+++ Семинары_C#/HW_6_SEM6/Task_2/Program.cs
@@ -0,0 +1,32 @@
+﻿// Задача 43: Напишите программу, которая найдёт точку пересечения двух прямых, 
+// заданных уравнениями y = k1 * x + b1, y = k2 * x + b2; значения b1, k1, b2 и k2 
+// задаются пользователем.
+
+// b1 = 2, k1 = 5, b2 = 4, k2 = 9 -> (-0,5; -0,5)
+
+double x;
+double y;
+
+Console.Write("Input k1 of the first linear equation: ");
+double k1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input b1 of the first linear equation: ");
+double b1 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input k2 of the second linear equation: ");
+double k2 = Convert.ToInt32(Console.ReadLine());
+Console.Write("Input b2 of the second linear equation: ");
+double b2 = Convert.ToInt32(Console.ReadLine());
+
+if (k1 == k2 & b1 != b2)
+{
+    Console.WriteLine("Lines are parallel");
+}
+if (k1 == k2 & b1 == b2)
+{
+    Console.WriteLine("The same equations");
+}
+if(k1 != k2)
+{
+x = (b2 - b1) / (k1 - k2);
+y = k1 * x + b1;
+Console.WriteLine("Intersection point coordinates: ( " + x + " ; " + y + " )");
+}
